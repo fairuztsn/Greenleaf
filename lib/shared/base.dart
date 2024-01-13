da42cf115362b88a68d718439067fadf1bbe861b
Gede Krisna <igkganes@gmail.com>
2024-01-13 20:45:27 +0700

    Added new base file

---
+++ lib/shared/base.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Base1 extends StatefulWidget {
+  const Base1({super.key});
+
+  @override
+  State<Base1> createState() => _Base1State();
+}
+
+class _Base1State extends State<Base1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Base2 extends StatefulWidget {
+  const Base2({super.key});
+
+  @override
+  State<Base2> createState() => _Base2State();
+}
+
+class _Base2State extends State<Base2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
