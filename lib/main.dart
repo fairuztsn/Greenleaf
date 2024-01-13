20b96c4b5441d36bb93f8b877cbdb125f1cccdfd
Gede Krisna <igkganes@gmail.com>
2024-01-13 20:45:13 +0700

    feat: redirect to onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:greenleaf/views/onboarding/onboarding.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:greenleaf/environment/env.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Color.fromARGB(255, 119, 244, 70),
+      statusBarIconBrightness: Brightness.light));
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   await Supabase.initialize(
-    url: Env.supabaseUrl, 
-    anonKey: Env.supabasePublicKey
-  );
-  runApp(const MyApp());
+      url: Env.supabaseUrl, anonKey: Env.supabasePublicKey);
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -16,35 +22,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GreenLeaf',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'GreenLeaf Home Page'),
-    );
+        title: 'GreenLeaf',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(fontFamily: "Mulish"),
+        home: ScrollConfiguration(
+          behavior: MyBehavior(),
+          child: const OnBoardingScreen(),
+        ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text("This is Home Page"),)
-    );
+class MyBehavior extends ScrollBehavior {
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
   }
 }
