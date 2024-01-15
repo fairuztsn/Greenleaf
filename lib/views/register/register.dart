9fef2da40eb371a4fe7ee90ea8fe8b671a3a9d26
Gede Krisna <igkganes@gmail.com>
2024-01-15 18:18:29 +0700

    feat: general register first step

--- lib/views/register/register.dart
+++ lib/views/register/register.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:greenleaf/shared/base.dart';
+import 'package:greenleaf/shared/const.dart';
+import 'package:greenleaf/views/register/second_step.dart/second_register.dart';
+import 'package:page_transition/page_transition.dart';
 
 class RegisterScreen extends ConsumerStatefulWidget {
   const RegisterScreen({super.key});
@@ -10,15 +13,143 @@ class RegisterScreen extends ConsumerStatefulWidget {
 }
 
 class _RegisterScreenState extends ConsumerState<RegisterScreen> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController phone = TextEditingController();
+  TextEditingController firstName = TextEditingController();
+  TextEditingController lastName = TextEditingController();
+  bool isPasswordObscured = true;
   final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return BaseApp.loginRegisterBackground(
-        states: "Daftar",
-        children: [Text("Ini Register Page")],
-        linkable: "Ada kendala?",
-        toLinkable: () {},
-        lanjut: () {},
-        formKey: formKey);
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: BaseApp.loginRegisterBackground(
+          states: "Daftar",
+          children: [
+            TextFormField(
+              validator: ((val) {
+                if (val!.isEmpty) {
+                  return 'Please Fill The Email Correctly!';
+                }
+                return null;
+              }),
+              controller: email,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 1, color: Colors.black),
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          width: 1, color: Constants.colorGreenLeaf),
+                      borderRadius: BorderRadius.circular(10)),
+                  label: const Text(
+                    "Email",
+                    style: TextStyle(fontSize: 14, color: Colors.black),
+                  )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              validator: ((val) {
+                if (val!.isEmpty) {
+                  return 'Please Fill The Phone Number Correctly!';
+                }
+                return null;
+              }),
+              controller: phone,
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(width: 1, color: Colors.black),
+                      borderRadius: BorderRadius.circular(10)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          width: 1, color: Constants.colorGreenLeaf),
+                      borderRadius: BorderRadius.circular(10)),
+                  label: const Text(
+                    "Phone",
+                    style: TextStyle(fontSize: 14, color: Colors.black),
+                  )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              validator: ((val) {
+                if (val!.isEmpty) {
+                  return 'Please Fill The Password';
+                }
+                return null;
+              }),
+              controller: password,
+              obscureText: isPasswordObscured,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(width: 1, color: Colors.black),
+                    borderRadius: BorderRadius.circular(10)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                        width: 1, color: Constants.colorGreenLeaf),
+                    borderRadius: BorderRadius.circular(10)),
+                label: const Text(
+                  "Password",
+                  style: TextStyle(fontSize: 14, color: Colors.black),
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      if (isPasswordObscured) {
+                        isPasswordObscured = false;
+                      } else {
+                        isPasswordObscured = true;
+                      }
+                    });
+                  },
+                  icon: Icon(
+                      isPasswordObscured == true
+                          ? Icons.visibility_off_rounded
+                          : Icons.visibility_rounded,
+                      color: Constants.colorGreenLeaf),
+                ),
+              ),
+            ),
+          ],
+          linkable: "Ada kendala?",
+          toLinkable: () {},
+          lanjut: () {
+            final navigator = Navigator.of(context);
+            if (formKey.currentState!.validate()) {
+              showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (context) => const Center(
+                        child: CircularProgressIndicator.adaptive(
+                          backgroundColor: Constants.colorGreenLeaf,
+                        ),
+                      ));
+              Future.delayed(const Duration(milliseconds: 1500), () {
+                navigator.pushReplacement(
+                  PageTransition(
+                      child: SecondRegisterScreen(
+                        email: email.text,
+                        phone: phone.text,
+                        password: password.text,
+                      ),
+                      type: PageTransitionType.rightToLeft),
+                );
+              });
+            }
+            setState(() {});
+          },
+          submit: "Lanjut",
+          formKey: formKey),
+    );
   }
 }
