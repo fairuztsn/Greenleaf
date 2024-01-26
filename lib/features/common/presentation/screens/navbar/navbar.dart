9eac51bc65d24db0445b735971e7b92c95a9599a
Gede Krisna <igkganes@gmail.com>
2024-01-26 12:58:39 +0700

    refactor: cleaning the code

--- lib/features/common/presentation/screens/navbar/navbar.dart
+++ lib/features/common/presentation/screens/navbar/navbar.dart
@@ -29,16 +29,20 @@ class _NavbarState extends ConsumerState<Navbar> {
     setState(() {
       selectedIndex = index;
     });
-    _pageController.animateToPage(selectedIndex,
-        duration: const Duration(milliseconds: 500), curve: Curves.easeOutQuad);
+    _pageController.animateToPage(
+      selectedIndex,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeOutQuad,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: const SystemUiOverlayStyle(
-          systemNavigationBarColor: Colors.white,
-          systemNavigationBarIconBrightness: Brightness.dark),
+        systemNavigationBarColor: Colors.white,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
       child: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.white,
@@ -51,45 +55,48 @@ class _NavbarState extends ConsumerState<Navbar> {
             decoration: BoxDecoration(
               boxShadow: [
                 BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    offset: const Offset(0, -10),
-                    blurRadius: 8.0)
+                  color: Colors.black.withOpacity(0.2),
+                  offset: const Offset(0, -10),
+                  blurRadius: 8,
+                ),
               ],
             ),
             child: GNav(
-                backgroundColor: AppColors.colorGreenLeaf,
-                haptic: true,
-                tabBorderRadius: 15, // tab button shadow
-                curve: Curves.easeOutExpo, // tab animation curves
-                duration:
-                    const Duration(milliseconds: 50), // tab animation duration
-                gap: 2, // the tab button gap between icon and text
-                color: Colors.white38, // unselected icon color
-                activeColor: Colors.white, // selected icon and text color
-                iconSize: 24, // tab button icon size
-                tabBackgroundColor: Colors.purple
-                    .withOpacity(0.1), // selected tab background color
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 20, vertical: 5), // navigation bar padding
-                tabs: const [
-                  GButton(
-                    margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                    icon: Icons.home,
-                    text: 'Beranda',
-                  ),
-                  GButton(
-                    margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                    icon: Icons.question_mark_rounded,
-                    text: 'Tanya',
-                  ),
-                  GButton(
-                    margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                    icon: Icons.person,
-                    text: 'Profilku',
-                  )
-                ],
-                selectedIndex: selectedIndex,
-                onTabChange: onButtonPressed),
+              backgroundColor: AppColors.colorGreenLeaf,
+              tabBorderRadius: 15, // tab button shadow
+              curve: Curves.easeOutExpo, // tab animation curves
+              duration:
+                  const Duration(milliseconds: 50), // tab animation duration
+              gap: 2, // the tab button gap between icon and text
+              color: Colors.white38, // unselected icon color
+              activeColor: Colors.white, // selected icon and text color
+              iconSize: 24, // tab button icon size
+              tabBackgroundColor: Colors.purple
+                  .withOpacity(0.1), // selected tab background color
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 5,
+              ), // navigation bar padding
+              tabs: const [
+                GButton(
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                  icon: Icons.home,
+                  text: 'Beranda',
+                ),
+                GButton(
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                  icon: Icons.question_mark_rounded,
+                  text: 'Tanya',
+                ),
+                GButton(
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                  icon: Icons.person,
+                  text: 'Profilku',
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onTabChange: onButtonPressed,
+            ),
           ),
         ),
       ),
